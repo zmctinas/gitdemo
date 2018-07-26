75908887c5efe228352963b43c245524997ceb2f
JieLi <zmctinas@163.com>
2018-07-26 17:58:34 +0800

    Initial Commit

---
+++ gitdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitdemo
+//
+//  Created by Andy on 2018/7/26.
+//  Copyright © 2018年 JieLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitdemo
+//
+//  Created by Andy on 2018/7/26.
+//  Copyright © 2018年 JieLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
